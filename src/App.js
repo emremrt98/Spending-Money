b8bf6b89de5354cd89c33f9a7b02b9bb56b3fae8
Emre Mert <emre.mrt98@gmail.com>
2022-08-18 01:21:22 +0300

    buying is active

--- src/App.js
+++ src/App.js
@@ -2,22 +2,77 @@ import './css/main.css';
 import Header from './components/header';
 import Product from './components/product';
 import Card from './components/card';
+import { faker } from '@faker-js/faker';
+
 function App() {
+
+  const production = [
+    {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 2000, 0, '$'),
+      img: faker.image.fashion()
+    },
+    {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 2000, 0, '$'),
+      img: faker.image.fashion()
+    },
+    {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 2000, 0, '$'),
+      img: faker.image.fashion()
+    },
+    {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 2000, 0, '$'),
+      img: faker.image.fashion()
+    },
+    {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 2000, 0, '$'),
+      img: faker.image.fashion()
+    },
+    {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 2000, 0, '$'),
+      img: faker.image.fashion()
+    },
+    {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 2000, 0, '$'),
+      img: faker.image.fashion()
+    },
+    {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 2000, 0, '$'),
+      img: faker.image.fashion()
+    },
+    {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 2000, 0, '$'),
+      img: faker.image.fashion()
+    },
+    {
+      name: faker.commerce.product(),
+      price: faker.commerce.price(100, 2000, 0, '$'),
+      img: faker.image.fashion()
+    }
+  ]
+
+
+  console.log(production);
   return (
     <div className="App">
       <Header />
       <div className='hizala'>
         <div className='production'>
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+          {
+            production.map((data, index) => (
+              <div key={index}>
+                <Product name={data.name} price={data.price} img={data.img} />
+              </div>)
+            )}
+
         </div>
         <div>
           <Card />
--- src/components/product.js
+++ src/components/product.js
@@ -1,19 +1,21 @@
+import { useState } from "react";
 
-export default function Product() {
+export default function Product({ name, price, img }) {
+    const [count, setCount] = useState(0);
     return (
         <div className="product">
             <div>
                 <div className="img">
-                    <img src="https://img-bizimtoptan.mncdn.com/Content/Images/Thumbs/1545393.png"></img>
+                    <img src={img} ></img>
                 </div>
                 <div className="info">
-                    <h3>Sakız</h3>
-                    <p>$ 0.5</p>
+                    <h3>{name}</h3>
+                    <p>{price}</p>
                 </div>
                 <div className="btn">
                     <button className="sell" disabled>Sat</button>
-                    <p>0</p>
-                    <button className="buy">Satın Al</button>
+                    <p>{count}</p>
+                    <button onClick={()=> setCount(count+1)} className="buy">Satın Al</button>
                 </div>
             </div>
         </div>
